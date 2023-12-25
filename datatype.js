7d4dbd1d65305506a93f36230f6e1e4d824f96b4
doerjun <rokakumar11@gmail.com>
2023-12-26 01:56:13 +0300

    datatypes javascript

---
+++ datatype.js
@@ -0,0 +1,16 @@
+"use strict" ;// treat all js code as newer version
+//alert('hello'); //alert works in browser not in nodee js
+let name='Jun' //string datatype
+let age=23 //number=>2 to power 53;
+let isLoggein=true; //boolean
+//null =>standalone value represents empty value which means its khali u khali xa
+//undefined=>value is not assigned
+//symbol=>uniqueness
+//primitive datatypes
+
+
+//objects
+console.log(typeof('ktm'));
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined));
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('this is js')
+console.log(4+5);
+const a=4;
+const b=5;
+console.log('the product is',a*b)
